85a9a597db968ba6311237df03cfab6cb6b39dec
PrashanthiNallapu <prashanthinallapu2002@gmail.com>
2025-08-22 17:52:24 +0530

    changed background colors

--- src/App.js
+++ src/App.js
@@ -1,72 +1,4 @@
 
-
-
-
-
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import HeroSection from "./components/HeroSection";
-// import CivilServices from "./components/CivilServices";
-// import ExpertGuidance from "./components/ExpertGuidance";
-// import TestSeries from "./components/TestSeries";
-// import ContentSection from "./components/ContentSection";
-// import Footer from "./components/Footer";
-// import AboutUs from "./components/AboutUs";
-// import Exams from "./components/Exams";
-// import SignupScreen from "./components/SignupScreen";
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />
-
-//       <div style={{ marginTop: "80px" }}>
-//         <Routes>
-//           {/* Home Page */}
-//           <Route
-//             path="/"
-//             element={
-//               <>
-//                 <HeroSection />
-//                 <CivilServices />
-//                 <TestSeries />
-//                 <ExpertGuidance />
-//                 <ContentSection />
-//                 <Footer />
-//               </>
-//             }
-//           />
-
-         
-//            <Route path="/exams" element={<Exams/>} />
-//           {/* <Route path="/test-series" element={<TestSeries/>} />  */}
-//           <Route path="/about" element={<AboutUs/>} />
-//           {/* <Route path="/contact" element={<Contact Us/>} /> */}
-//           <Route
-//             path="/login"
-//             element={<h2 style={{ textAlign: "center", marginTop: "150px" }}>Login Page</h2>}
-//           />
-//             <Route path="/signup" element={<SignupScreen />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -79,7 +11,7 @@ import Footer from "./components/Footer";
 import AboutUs from "./components/AboutUs";
 import Exams from "./components/Exams";
 
-// import PracticeSection from "./components/PracticeSection"; // 👈 new import
+
 
 import SignupScreen from "./components/SignupScreen";
 import LoginScreen from "./components/LoginScreen";
@@ -108,7 +40,6 @@ function AppLayout() {
                 <HeroSection />
                 <CivilServices />
                 <TestSeries />
-                {/* <PracticeSection /> 👈 Added here */}
                 <ExpertGuidance />
                 <ContentSection />
                 <Footer />
@@ -119,14 +50,7 @@ function AppLayout() {
           <Route path="/exams" element={<Exams />} />
           <Route path="/about" element={<AboutUs />} />
 
-          <Route
-            path="/login"
-            element={
-              <h2 style={{ textAlign: "center", marginTop: "150px" }}>
-                Login Page
-              </h2>
-            }
-          />
+      
           <Route path="/login"
             element={<LoginScreen/>}
           />
